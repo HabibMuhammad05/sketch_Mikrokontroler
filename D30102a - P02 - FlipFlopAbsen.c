657db5d6839bf6fd99a094ba4117d114766f23ac
Habib Muhammad <101261125+HabibMuhammad05@users.noreply.github.com>
2024-12-19 15:37:50 +0700

    Update D30102a - P02 - FlipFlopAbsen.c
    Apdent

--- D30102a - P02 - FlipFlopAbsen.c
+++ D30102a - P02 - FlipFlopAbsen.c
@@ -21,18 +21,18 @@ Komponen:
 #include <mega8535.h> 
 #include <delay.h>
 
-char absen10 = 00001010;
+char absen10 = 0b00001010;
 
 void main(void)
 { 
-  PORTD = 0xFF;
+  DDRD = 0xFF;
     
 while (1)
     {    
-      DDRD = absen10;
+      PORTD = absen10;
       delay_ms(1000);
       
-      DDRD = 0x00;  
+      PORTD = 0x00;  
       delay_ms(1000);
     }
 }
